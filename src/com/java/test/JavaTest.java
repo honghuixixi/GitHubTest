290b15c28514463ec6c588b31e29b5610d537dbd
honghui <775706122@qq.com>
2016-03-16 16:40:26 +0800

    fix2

--- src/com/java/test/JavaTest.java
+++ src/com/java/test/JavaTest.java
@@ -3,7 +3,9 @@ package com.java.test;
 public class JavaTest {
 
 	public static void main(String[] args) {
+		System.out.println("===========================");
 		System.out.println("This is  a GitHub test!");
+		System.out.println("===========================");
 	}
 
 }
